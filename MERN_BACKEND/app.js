c95e744c20b83250c2f9f054958523d9934cd07e
Gokul Santhosh <gokulsanthosh125@gmail.com>
2025-01-23 22:27:53 +0530

    Update app.js

--- MERN_BACKEND/app.js
+++ MERN_BACKEND/app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const employeeRoutes = require('./routes/employeeroutes'); 
+const employeeRoutes = require('./routes/employeeroutes');
+const adminRoutes = require('./routes/adminroutes'); 
 
 const app = express();
 
-
-app.use(cors()); 
-app.use(express.json()); 
-
+app.use(cors());
+app.use(express.json());
 
 mongoose.connect('mongodb+srv://gokku:gokku_7025@gokku.cz4bx.mongodb.net/mernsdb?retryWrites=true&w=majority&appName=GOKKU', {
   useNewUrlParser: true,
@@ -17,8 +16,8 @@ mongoose.connect('mongodb+srv://gokku:gokku_7025@gokku.cz4bx.mongodb.net/mernsdb
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log('MongoDB connection error:', err));
 
-
 app.use('/api', employeeRoutes);
+app.use('/api/admin', adminRoutes); 
 
 const PORT = 5000;
 app.listen(PORT, () => {
