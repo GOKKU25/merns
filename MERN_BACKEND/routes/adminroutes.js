6a4e8cc49febc4f405b741b82fb9b5ef91baeaf3
Gokul Santhosh <gokulsanthosh125@gmail.com>
2025-01-23 22:26:09 +0530

    Update adminroutes.js

--- MERN_BACKEND/routes/adminroutes.js
+++ MERN_BACKEND/routes/adminroutes.js
@@ -3,25 +3,33 @@ const jwt = require('jsonwebtoken');
 const AdminData = require('../models/admindata');
 const router = express.Router();
 
-
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
+  console.log('Received login request with:', { username, password }); 
+
   try {
+   
     const admin = await AdminData.findOne({ username });
 
-    if (!admin) return res.status(400).json({ message: 'Admin not found' });
-
-
+    if (!admin) {
+      console.log('Admin not found');
+      return res.status(400).json({ message: 'Admin not found' });
+    }
     if (admin.password !== password) {
+      console.log('Invalid password');
       return res.status(400).json({ message: 'Invalid credentials' });
     }
+    const token = jwt.sign(
+      { adminId: admin._id, role: admin.role },
+      process.env.JWT_SECRET || 'abcdefg',
+      { expiresIn: '1h' }
+    );
 
- 
-    const token = jwt.sign({ adminId: admin._id, role: admin.role }, process.env.JWT_SECRET || 'abcdefg', { expiresIn: '1h' });
-
-    res.json({ token });
+    console.log('Login successful');
+    res.json({ token }); 
   } catch (err) {
+    console.error('Error logging in:', err);
     res.status(500).json({ message: 'Error logging in' });
   }
 });
